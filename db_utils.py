f6f2746adc2255a5e326c8b505f20c55770dd6a1
mathias-wb <mathias.wb20@gmail.com>
2023-11-20 13:52:00 +0000

    Adds docstring and Pandas

--- db_utils.py
+++ db_utils.py
@@ -1,2 +1,21 @@
+import yaml
+
+"""
+Here I'm going to perform exploratory data analysis on a loan 
+portfolio, using various statistical and data visualisation 
+techniques to uncover patterns, relationships, and anomalies in 
+the loan data.
+
+This information will enable the business to make more informed 
+decisions about loan approvals, pricing, and risk management.
+
+By conducting exploratory data analysis on the loan data, 
+I'll aim to gain a deeper understanding of the risk and return 
+associated with the business' loans.
+
+Ultimately, my goal is to improve the performance and 
+profitability of the loan portfolio,
+"""
+
 class RDSDatabaseConnector:
     pass
