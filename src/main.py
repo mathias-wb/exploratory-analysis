34d9b8cd7a9c24003db30eea4bd7d1d7e9b52181
mathias-wb <mathias.wb20@gmail.com>
2023-12-05 22:33:05 +0000

    Adds skew correction.

--- src/main.py
+++ src/main.py
@@ -1,16 +1,16 @@
+from scipy.stats import skew, boxcox, yeojohnson
+
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import missingno as msno
 
-import matplotlib
+import math
 import db_utils
 
 pd.set_option("display.max_columns", None)
 pd.set_option("display.max_rows", None)
-
-df = db_utils.df_from_csv()
 
 # === Step 1. Transforming Data ===
 
@@ -34,10 +34,10 @@ class DataTransform:
         """
         self.df = dataframe
         self.remove_excess()
-        self.convert_to_int()
         self.convert_to_datetime()
         self.convert_to_bool()
         self.convert_to_category()
+        self.convert_to_int()
 
     def remove_excess(self):
         """
@@ -47,12 +47,18 @@ class DataTransform:
         self.df["term"] = self.df["term"].str.replace(" months", "").astype("Int64")
         self.df["verification_status"] = self.df["verification_status"] != "Not Verified"
 
-    def convert_to_int(self):
+    def convert_to_int(self, dataframe=None):
         """
         Converts columns that had unnecessary float values to integers.
         """
-        for col in ["sub_grade", "loan_amount", "funded_amount", "mths_since_last_delinq", "mths_since_last_record", "collections_12_mths_ex_med", "mths_since_last_major_derog"]:
-            self.df[col] = pd.to_numeric(self.df[col], errors="coerce").astype("Int64")
+        if dataframe is None:
+            for col in ["sub_grade", "loan_amount", "funded_amount", "mths_since_last_delinq", "mths_since_last_record", "collections_12_mths_ex_med", "mths_since_last_major_derog"]:
+                self.df[col] = pd.to_numeric(self.df[col], errors="coerce").astype("Int64")
+                return self.df
+        else:
+            for col in ["sub_grade", "loan_amount", "funded_amount", "mths_since_last_delinq", "mths_since_last_record", "collections_12_mths_ex_med", "mths_since_last_major_derog"]:
+                dataframe[col] = pd.to_numeric(dataframe[col], errors="coerce").astype("Int64")
+                return dataframe
 
     def convert_to_category(self):
         """
@@ -152,6 +158,16 @@ class DataFrameInfo:
         else:
             return f"{sum(self.df[column].isna())} ({round(sum(self.df[column].isna()) * 100 / self.get_shape()[0], sig_figures)}%)"
 
+    def get_skew(self, column=None, sig_figures=2):
+        if column is None:
+            output = f"=== Skew ===\n"
+            for col in self.df:
+                if self.df[col].dtype not in ["category", "datetime64[ns]", "bool"]:
+                    output += f"[{col}] : {round(skew(self.df[col]), sig_figures)}\n"
+            return output[:-1]
+        else:
+            return f"[{column}] : {round(skew(self.df[column]), sig_figures)}"
+
     def get_description(self):
         return self.df.describe()
     
@@ -171,6 +187,7 @@ class DataFrameTransform:
     """
     def __init__(self, dataframe):
         self.df = dataframe
+        self.original_df = dataframe.copy()
 
         # Columns with more than 50% null values get dropped.
         self.drop_columns(["mths_since_last_delinq", "mths_since_last_record", "next_payment_date", "mths_since_last_major_derog"])
@@ -182,6 +199,8 @@ class DataFrameTransform:
         self.impute_mean_columns(["collections_12_mths_ex_med"])
 
         self.drop_rows(["term", "int_rate", "employment_length"])
+
+        self.fix_skews()
 
     def drop_columns(self, columns:list):
         """Drops specified columns from the dataframe.
@@ -210,21 +229,54 @@ class DataFrameTransform:
         for col in columns:
             self.df = self.df[~self.df[col].isna()]
 
+    def fix_skews(self):
+        """Fixes skewed columns by applyin Log, Box-Cox or Yeo-Johnson transformation.
+        """
+        for col in self.df:
+            if self.df[col].dtype not in ["category", "datetime64[ns]", "bool"]:
+                if -0.5 < skew(self.df[col].astype("float64")) > 0.5:  # a common skew threshold to filter in the skewed columns
+                    log = self.df[col].map(lambda x: np.log(x) if x > 0 else 0)
+                    if self.df[col].all() > 0:
+                        box = pd.Series(boxcox(self.df[col].astype("float64"))[0])
+                    else:
+                        box = pd.Series(yeojohnson(self.df[col].astype("float64"))[0])
+                    self.df[col] = log if abs(skew(log) < abs(skew(box))) else box  # chooses the closest to 0
+
+    def remove_outliers(self, columns:list):  # TODO add outlier removal
+        pass
 
 class Plotter:
     def __init__(self, dataframe):
         self.df = dataframe
 
-    def null_difference(self, new_df):
-        fig, axes = plt.subplots(2, 1, figsize=(50, 50))
-        msno.bar(self.df, ax=axes[0], color="darkred")
-        msno.bar(new_df, ax=axes[1], color="green")
+    def null(self, comparison_df=None):
+        if comparison_df is None:
+            fig = plt.figure(figsize=(30, 30))
+            msno.bar(self.df, color="green")
+        else:
+            fig, axes = plt.subplots(2, 1, figsize=(50, 50))
+            msno.bar(comparison_df, ax=axes[0], color="darkred")
+            msno.bar(self.df, ax=axes[1], color="green")
         plt.show()
+        
+        
+    def distribution(self):  # TODO add comparison to original data
+        fig = plt.figure(figsize=(30, 30))
+        for col in self.df:
+            if self.df[col].dtype not in ["category", "datetime64[ns]", "bool"]:
+                sns.displot(self.df[col], color="green")
+                plt.show()
 
-# === A. Transforming Data ===
-df = DataTransform(df).df
-plot_df = Plotter(df)
+        
 
-df = DataFrameTransform(df).df
-info_df = DataFrameInfo(df)
-plot_df.null_difference(df)
+df = db_utils.df_from_csv()
+dt = DataTransform(df)
+dft = DataFrameTransform(dt.df)
+
+dfi = DataFrameInfo(df)
+dfti = DataFrameInfo(dft.df)
+
+plot_df = Plotter(dft.df)
+plot_df.distribution()
+
+# ===
