d69e726c04885a2a98d2ed7d74784240c60d5f1a
mathias-wb <mathias.wb20@gmail.com>
2023-12-06 18:12:34 +0000

    Restructures program to be more modular.

--- src/main.py
+++ src/main.py
@@ -1,5 +1,5 @@
 from scipy.stats import skew, boxcox, yeojohnson
-
+from datetime import datetime
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -10,15 +10,12 @@ import math
 import db_utils
 
 pd.set_option("display.max_columns", None)
-pd.set_option("display.max_rows", None)
-
-# === Step 1. Transforming Data ===
 
 class DataTransform:
     """This class is used to convert data into formats that are more 
     suitable for analysis.
     """
-    def remove_excess(self, df:pd.DataFrame) -> pd.DataFrame:
+    def remove_excess(self, df:pd.DataFrame) -> pd.DataFrame:  # NOTE this is hardcoded, might be a better way.
         """Cleans data within columns that require specific cleaning.
 
         Parameters
@@ -45,13 +42,13 @@ class DataTransform:
         df `pd.DataFrame` - 
             The dataframe with columns to be converted.
         """
-        df = self.convert_to_bool(df)
-        df = self.convert_to_category(df)
+        df = self.convert_to_boolean(df)
         df = self.convert_to_datetime(df)
-        df = self.convert_to_int(df)
+        df = self.convert_to_integer(df)
+        df = self.convert_to_category(df)
         return df
 
-    def convert_to_int(self, df:pd.DataFrame) -> pd.DataFrame:
+    def convert_to_integer(self, df:pd.DataFrame) -> pd.DataFrame:
         """ Converts columns that had whole float values to integers.
 
         Parameters
@@ -110,7 +107,7 @@ class DataTransform:
                 pass
         return df
 
-    def convert_to_bool(self, df:pd.DataFrame) -> pd.DataFrame:  # NOTE Could change this in the future, but not necessary right now.
+    def convert_to_boolean(self, df:pd.DataFrame) -> pd.DataFrame:  # NOTE Could change this in the future, but not necessary right now.
         """ Converts columns that have up to 2 values to boolean.
 
         Parameters
@@ -127,11 +124,16 @@ class DataTransform:
         df["policy_code"] = df["policy_code"] == 1
         return df
 
-# === Step 2. Get Information ===
 
 class DataFrameInfo:
     """This class is used to get information about the dataframe.
     """
+    def get_description(self, df:pd.DataFrame, sig_figures:int=2):
+        return round(df.describe(), sig_figures)
+    
+    def get_info(self, df:pd.DataFrame):
+        return df.info()
+
     def get_shape(self, df:pd.DataFrame) -> tuple:
         """Returns the shape of the dataframe.
 
@@ -163,7 +165,7 @@ class DataFrameInfo:
         `str` -
             The data type of the column in the dataframe.
         """
-        return df[column].dtype
+        return str(df[column].dtype)
 
     def get_datatypes(self, df:pd.DataFrame) -> pd.Series:
         """Returns the data types of the columns in the dataframe.
@@ -180,7 +182,7 @@ class DataFrameInfo:
         """
         return df.dtypes
 
-    def get_median(self, df:pd.DataFrame, column:str) -> int | float | datetime64[ns] | None:
+    def get_median(self, df:pd.DataFrame, column:str) -> int | float | datetime | None:
         """Returns the median of the column in the dataframe.
 
         Parameters
@@ -211,10 +213,10 @@ class DataFrameInfo:
         """
         output = f"=== Medians ===\n"
         for col in df:
-            output += f"{col}: {get_median(df, col)}\n"
+            output += f"{col}: {self.get_median(df, col)}\n"
         print(output[:-1])
 
-    def get_mean(self, df:pd.DataFrame, column:str, sig_figures:int=2) -> int | float | datetime64[ns] | bool | None:
+    def get_mean(self, df:pd.DataFrame, column:str, sig_figures:int=2) -> int | float | datetime | bool | None:
         """Returns the mean of the column in the dataframe.
 
         Parameters
@@ -266,7 +268,7 @@ class DataFrameInfo:
     def get_standard_deviations(self, df:pd.DataFrame):
         output = f"=== Standard Deviations ===\n"
         for col in df:
-            if df[column].dtype == "category":
+            if df[col].dtype == "category":
                 output += f"{col}: {self.get_standard_deviation(df, col)}\n"
         return output[:-1]
 
@@ -276,133 +278,143 @@ class DataFrameInfo:
     def get_distincts(self, df:pd.DataFrame):
         output = f"=== Unique Values ===\n"
         for col in df:
-            output += f"{col}: {self.get_distinct(df, col)} {list(df[col].unique())}\n"
+            if df[col].dtype == "category":
+                output += f"{col}: {self.get_distinct(df, col)} {list(df[col].unique())}\n"
         print(output[:-1])
 
-    
+    def get_null(self, df:pd.DataFrame, column:str, sig_figures:int=2) -> tuple[int, int|float]:
+        return (sum(df[column].isna()), 
+            round(sum(df[column].isna()) * 100 / self.get_shape(df)[0], sig_figures))
 
-    def get_null(self, df:pd.DataFrame, column:str, sig_figures:int=2):
-        return (sum(self.df[column].isna()), 
-            round(sum(self.df[column].isna()) * 100 / self.get_shape()[0], sig_figures))
+    def get_nulls(self, df:pd.DataFrame, sig_figures:int=2):
+        output = f"=== Null Values ===\n" 
+        for col in df:
+            if sum(df[col].isna()) != 0:
+                null_count, null_percent = self.get_null(df, col)
+                output += f"{col}: {null_count} ({null_percent}%)\n"
+        print(output[:-1])
 
-    def get_nulls(self, column=None, sig_figures=2):
-        output = f"=== Null Values ===\n"
-        for col in self.df:
-            if sum(self.df[col].isna()) != 0:
-                output += f"{col}: {sum(self.df[col].isna())} ({round(sum(self.df[col].isna()) * 100 / self.get_shape()[0], sig_figures)}%)\n"
-        return output[:-1]
-        else:
-            
+    def get_skew(self, df:pd.DataFrame, column:str, sig_figures:int=2) -> int | float:
+        try:
+            return round(skew(df[column].fillna(0)), sig_figures)  # FIXME Needs to convert column to int64 to work and then back to Int64
+        except ValueError:
+            return None
 
-    def get_skew(self, column=None, sig_figures=2):
-        if column is None:
-            for col in self.df:
-                if self.df[col].dtype not in ["category", "datetime64[ns]", "bool"]:
-                    output += f"[{col}] : {round(skew(self.df[col]), sig_figures)}\n"
-            return output[:-1]
-        else:
-            return f"[{column}] : {round(skew(self.df[column]), sig_figures)}"
+    def get_skews(self, df:pd.DataFrame, sig_figures:int=2):
+        output = f"=== Skews ===\n"
+        for col in df:
+            if df[col].dtype in ["Int64", "float64"]:
+                output += f"[{col}] : {self.get_skew(df, col)}\n"
+        print(output[:-1])
 
-    def get_description(self):
-        return self.df.describe()
-    
-    def get_info(self):
-        return self.df.info()
-
-
-# === Step 3. Removing and Imputing Missing Values ===
 
 class DataFrameTransform:
     """Initializes DataFrameTransform with a dataframe.
     """
-    def __init__(self):
-        self.df = dataframe
-        self.original_df = dataframe.copy()
-
-        # Columns with more than 50% null values get dropped.
-        self.drop_columns(["mths_since_last_delinq", "mths_since_last_record", "next_payment_date", "mths_since_last_major_derog"])
-
-        # Columns that have null values that are likely should be zeros are filled with zeros.
-        self.fill_zero_columns(["funded_amount"])
-        
-        self.impute_median_columns(["last_payment_date", "last_credit_pull_date"])
-        self.impute_mean_columns(["collections_12_mths_ex_med"])
-
-        self.drop_rows(["term", "int_rate", "employment_length"])
-
-        self.fix_skews()
-
-    def drop_columns(self, columns:list):
-        """Drops specified columns from the dataframe.
-
-        Parameters
-        ----------
-        columns : (list)
-            The column names to drop.
+    def drop_columns(self, df:pd.DataFrame, threshold:int=50):
+        """Drops columns from the dataframe with more nulls than the threshold percentage.
         """
-        for col in columns:
-            self.df = self.df.drop(col, axis=1)
+        for col in df:
+            if DataFrameInfo().get_null(df, col)[1] > threshold / 100:
+                df = df.drop(col, axis=1)
+        return df
 
-    def fill_zero_columns(self, columns:list):
+    def fill_zero_columns(self, df:pd.DataFrame, columns:list):
         for col in columns:
-            self.df[col] = self.df[col].fillna(0)
+            df[col] = df[col].fillna(0)
+        return df
 
-    def impute_median_columns(self, columns:list):
+    def impute_median_columns(self, df:pd.DataFrame, columns:list):
         for col in columns:
-            self.df[col] = self.df[col].fillna(self.df[col].median())
+            df[col] = df[col].fillna(df[col].median())
+        return df
 
-    def impute_mean_columns(self, columns:list, sig_figures=2):
+    def impute_mean_columns(self, df:pd.DataFrame, columns:list, sig_figures=2):
         for col in columns:
-            self.df[col] = self.df[col].fillna(round(self.df[col].mean(), 2))
+            df[col] = df[col].fillna(round(df[col].mean(), 2))
+        return df
 
-    def drop_rows(self, columns:list):
+    def drop_rows(self, df:pd.DataFrame, columns:list):
         for col in columns:
-            self.df = self.df[~self.df[col].isna()]
+            df = df[~df[col].isna()]
+        return df
 
-    def fix_skews(self):
+    def fix_skews(self, df:pd.DataFrame):
         """Fixes skewed columns by applyin Log, Box-Cox or Yeo-Johnson transformation.
         """
-        for col in self.df:
-            if self.df[col].dtype not in ["category", "datetime64[ns]", "bool"]:
-                if -0.5 < skew(self.df[col].astype("float64")) > 0.5:  # a common skew threshold to filter in the skewed columns
-                    log = self.df[col].map(lambda x: np.log(x) if x > 0 else 0)
-                    if self.df[col].all() > 0:
-                        box = pd.Series(boxcox(self.df[col].astype("float64"))[0])
+        for col in df:
+            if df[col].dtype not in ["category", "datetime64[ns]", "bool"]:
+                if -0.5 < skew(df[col].astype("float64")) > 0.5:  # a common skew threshold to filter in the skewed columns
+                    log = df[col].map(lambda x: np.log(x) if x > 0 else 0)
+                    if df[col].all() > 0:
+                        box = pd.Series(boxcox(df[col].astype("float64"))[0])
                     else:
-                        box = pd.Series(yeojohnson(self.df[col].astype("float64"))[0])
-                    self.df[col] = log if abs(skew(log) < abs(skew(box))) else box  # chooses the closest to 0
+                        box = pd.Series(yeojohnson(df[col].astype("float64"))[0])
+                    df[col] = log if abs(skew(log) < abs(skew(box))) else box  # chooses the closest to 0
+        
+        return df
 
     def remove_outliers(self, columns:list):  # TODO add outlier removal
         pass
 
 class Plotter:
-    def __init__(self, dataframe):
-        self.df = dataframe
-
-    def null(self, comparison_df=None):
+    def null(self, df:pd.DataFrame, comparison_df:pd.DataFrame=None):
         if comparison_df is None:
             fig = plt.figure(figsize=(30, 30))
-            msno.bar(self.df, color="green")
+            msno.bar(df, color="green")
         else:
             fig, axes = plt.subplots(2, 1, figsize=(50, 50))
             msno.bar(comparison_df, ax=axes[0], color="darkred")
-            msno.bar(self.df, ax=axes[1], color="green")
+            msno.bar(df, ax=axes[1], color="green")
         plt.show()
         
         
-    def distribution(self):  # TODO add comparison to original data
+    def distribution(self, df:pd.DataFrame):  # TODO add comparison to original data
         fig = plt.figure(figsize=(30, 30))
-        for col in self.df:
-            if self.df[col].dtype not in ["category", "datetime64[ns]", "bool"]:
-                sns.displot(self.df[col], color="green")
+        for col in df:
+            if df[col].dtype not in ["category", "datetime64[ns]", "bool"]:
+                sns.displot(df[col], color="green")
                 plt.show()
 
-        
 
-df = db_utils.df_from_csv()
+if __name__ == "__main__":
+    df = db_utils.df_from_csv()
 
-# 1. Convert column format.
-df = DataTransform().remove_excess(df)
-df = DataTransform().convert_column_formats(df)
+    # 1. Convert column format
+    df = DataTransform().remove_excess(df)
+    df = DataTransform().convert_column_formats(df)
 
-# ===
+    # 2. Data information
+    df_info = DataFrameInfo()
+
+    # df_info.get_info(df)
+    # df_info.get_description(df)  
+    # df_info.get_datatypes(df)
+    # df_info.get_shape(df)
+
+    # df_info.get_distincts(df)
+    # df_info.get_means(df)
+    # df_info.get_medians(df) 
+    # df_info.get_modes(df)
+    # df_info.get_nulls(df)
+    # df_info.get_standard_deviations(df)
+
+    # 3. Remove / impute data
+    df_transform = DataFrameTransform()
+    original_df = df.copy()
+    
+    df = df_transform.fill_zero_columns(df, ["funded_amount"])
+    df = df_transform.impute_mean_columns(df, ["collections_12_mths_ex_med"])
+    df = df_transform.impute_median_columns(df, ["last_payment_date", "last_credit_pull_date"])
+    df = df_transform.drop_rows(df, ["term", "int_rate", "employment_length"])
+    df = df_transform.drop_columns(df)
+    
+    # 4. Transform skewed data
+    plot = Plotter()
+    df = df_transform.fix_skews(df)
+    
+    # plot.null(df, original_df)
+
+    plot.distribution(df)
+
+
