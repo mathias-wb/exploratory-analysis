31cee7951838839b74283666d71c1832a6c91411
mathias-wb <mathias.wb@20@gmail.com>
2023-12-12 16:47:34 +0000

    Add visualisation of loan recovery.

--- src/analysis.py
+++ src/analysis.py
@@ -5,47 +5,69 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-import db_utils, cleaning
-
-# 1. Summarise what percentage of the loans are recovered against the 
-# investor funding and the total amount funded. 
-# Visualise your results on an appropriate graph.
-def percentage_of_loans_recovered(df: pd.DataFrame):
-        total_funded = df["funded_amount"].sum()
-        total_recovered = df["total_payment"].sum()
-        total_funded_inv = df["funded_amount_inv"].sum()
-        total_recovered_inv = df["total_payment_inv"].sum()
-
-        percent_recovered_total = (total_recovered / total_funded) * 100
-        percent_recovered_inv = (total_recovered_inv / total_funded_inv) * 100
-        
-        data = {
-            "Category": ["Against\nTotal Funding", "Against\nInvestor Funding"],
-            "Percentage Recovered": [percent_recovered_total, percent_recovered_inv]
-        }
-        df_loan_summary = pd.DataFrame(data)
-
-        sns.set_theme(style="whitegrid", font="JetBrains Mono")
-        plt.figure(figsize=(5,6))
-        ax = sns.barplot(data=df_loan_summary, x="Category", y="Percentage Recovered")
-        plt.title("Percentage of Loans Recovered")
-        plt.ylabel("Percentage (%)")
-        plt.ylim([0,100])
-        plt.xlabel(None)
-        
-        for bar in ax.patches: # placing percentage text on bars
-            ax.annotate(f'{round(bar.get_height(), 2)}%', (bar.get_x() + bar.get_width() / 2, bar.get_height()),
-                    ha="center", va='center', xytext=(0, -20), textcoords="offset points", color="white")
-        plt.show()
+import math, db_utils, cleaning
 
 
-#1. (cont.) Additionally visualise what percentage of the total amount 
-# would be recovered up to 6 months' in the future.
-def percentage_of_loans_recovered_in_6_months(df: pd.DataFrame):
-    pass
+def percentage_of_loan_recovery(df: pd.DataFrame) -> None:
+    """Calculates and visualizes percentage of loans recovered against total funding and investor funding.
+
+    This function takes a pandas DataFrame containing loan data, calculates recovery percentages against total and investor funding, 
+    projects recovery percentage in 6 months, and visualizes the results as a bar chart.
+
+    Parameters
+    ----------
+    df : pandas DataFrame
+        DataFrame containing loan data 
+    """
+    
+    total_funded = df["funded_amount"].sum()
+    total_recovered = df["total_payment"].sum()
+    total_funded_inv = df["funded_amount_inv"].sum()
+    total_recovered_inv = df["total_payment_inv"].sum()
+
+    percent_recovered_total = (total_recovered / total_funded) * 100
+    percent_recovered_inv_total = (total_recovered_inv / total_funded_inv) * 100
+    
+    df["months_left"] = df["out_prncp"] / df["instalment"]
+    df["months_left_inv"] = df["out_prncp_inv"] / df["instalment"]
+    df["amount_recovered_in_6_mths"] = np.where(df["months_left"] < 6, df["months_left"] * df["instalment"], 6 * df["instalment"])
+    df["amount_recovered_in_6_mths_inv"] = np.where(df["months_left_inv"] < 6, df["months_left_inv"] * df["instalment"], 6 * df["instalment"])
+    
+    total_recovered_in_6_mths = df["amount_recovered_in_6_mths"].sum()
+    total_recovered_in_6_mths_inv = df["amount_recovered_in_6_mths_inv"].sum()
+    
+    percent_total_recovered_in_6_mths = (total_recovered_in_6_mths / total_funded) * 100
+    percent_total_recovered_in_6_mths_inv = (total_recovered_in_6_mths_inv / total_funded_inv) * 100
+
+    data = {
+        "Category": ["x", "y", "Against\nTotal Funding", "Against\nInvestor Funding"],
+        "Percentage Recovered": [percent_recovered_total, percent_recovered_inv_total, percent_total_recovered_in_6_mths, percent_total_recovered_in_6_mths_inv]
+    }
+    df_loan_summary = pd.DataFrame(data)
+
+    sns.set_theme(style="whitegrid", font="JetBrains Mono")
+    plt.figure(figsize=(9,16))
+
+    # Plot the bars for recovered funding
+    ax = sns.barplot(data=df_loan_summary.iloc[:2], x="Category", y="Percentage Recovered", label=["Recovered Total Funding", "Recovered Investor Funding"])
+    # Plot the stacked bars for projected recoveries
+    ax = sns.barplot(data=df_loan_summary.iloc[2:], x="Category", y="Percentage Recovered", bottom=df_loan_summary.iloc[:2]["Percentage Recovered"], color="#76ae55", label="Projected Recovery in 6 Months")
+    
+    plt.title("Percentage of Loans Recovered")
+    plt.ylabel("Percentage (%)")
+    plt.xlabel(None)
+    
+    # Write the percentage of the portion in the centre of the each stacked bar.
+    for bar, label in zip(ax.patches, df_loan_summary["Percentage Recovered"]):
+        height = bar.get_height() + bar.get_y()
+        ax.annotate(f'{round(label, 2)}%', (bar.get_x() + bar.get_width() / 2, (bar.get_y() + height) / 2),
+                    ha="center", va='center', xytext=(0, 0), textcoords="offset points", color="white")
+    
+    plt.legend()
+    plt.show()
+
 
 
 df = cleaning.clean_data(db_utils.df_from_csv("loans.csv"))
 
-percentage_of_loans_recovered(df)
-display(df)
+percentage_of_loan_recovery(df)
